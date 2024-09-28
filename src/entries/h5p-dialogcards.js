cb9ab0fafc19ace1b2ebaaee9ace6a5b957b13fe
Olivier Swaak <o.swaak@hotmail.com>
2024-09-28 22:05:22 +0200

    feat: new changes

--- src/entries/h5p-dialogcards.js
+++ src/entries/h5p-dialogcards.js
@@ -1,5 +1,5 @@
 import "../styles/h5p-dialogcards.css";
-import DialogcardsCode from "../scripts/h5p-dialogcards.js";
+import DialogcardsCode from "../scripts/h5p-dialogcardscode.js";
 
 // Load library
 // H5P = H5P || {};
